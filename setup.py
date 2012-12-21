68f5b505da62f5fa03b21a811b8730627a5c702e
DavidSperling <ongakujin@gmail.com>
2012-12-21 02:49:06 -0500

    Created a .defaultConfig file that is used in the repository and shipped with the product. The idea is that users create a config file from it during setup, and they can modify it without causing any problems with the repository.

--- setup.py
+++ setup.py
@@ -6,13 +6,17 @@
 # getting user credentials.
 
 import json
+from subprocess import call
+
+# create the default configuration file
+call(["cp", ".defaultConfig", "config"])
 
 # Get the user's gmail credentials.
 gmailName = raw_input('Enter your gmail username:')
 gmailPass = raw_input('Enter your gmail password:')
 
 # Open the json file to hold the credentials
-jsonFile = open('gmailParser/.gmail.json', 'w+')
+jsonFile = open('etc/.gmail.json', 'w+')
 
 # Encode the credentials into json
 jsonString = json.dumps({'gmailName': gmailName, 'gmailPass':gmailPass})
