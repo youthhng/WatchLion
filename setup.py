76fc533862b06a46643e55b4acfd6f69807e9cd7
DavidSperling <ongakujin@gmail.com>
2012-12-21 23:19:26 -0500

    setup.py now collects the userName and password for eLion. Instead of saving user credentials in etc/.gmail.json, they are now stored in etc/.credentials.json.

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@
 # getting user credentials.
 
 import json
+import getpass
 from subprocess import call
 
 # create the default configuration file
@@ -13,13 +14,15 @@ call(["cp", ".defaultConfig", "config"])
 
 # Get the user's gmail credentials.
 gmailName = raw_input('Enter your gmail username:')
-gmailPass = raw_input('Enter your gmail password:')
+gmailPass = getpass.getpass('Enter your gmail password:')
+eLionName = raw_input('Enter your eLion username:')
+eLionPass = getpass.getpass('Enter your eLion password:')
 
 # Open the json file to hold the credentials
-jsonFile = open('etc/.gmail.json', 'w+')
+jsonFile = open('etc/.credentials.json', 'w+')
 
 # Encode the credentials into json
-jsonString = json.dumps({'gmailName': gmailName, 'gmailPass':gmailPass})
+jsonString = json.dumps({'gmailName':gmailName,'gmailPass':gmailPass,'eLionName':eLionName,'eLionPass':eLionPass})
 
 # Write the jsonString to the file
 jsonFile.write(jsonString)
